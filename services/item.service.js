cb12f9c237d1921967d6959a266533a225626e8d
Zack Kramer <zckramer83@gmail.com>
2019-12-07 17:59:16 -0500

    friday patch fixes 2

--- services/item.service.js
+++ services/item.service.js
@@ -9,15 +9,8 @@ module.exports = {
     },
  
     editItemAssignedTo(itemId, userId, callback) {     
-<<<<<<< HEAD
             const Item = ItemModel.findById(itemId); // need the entire model?
             Item.findOneAndUpdate(itemId, { assignedTo: userId }, {new: true})
-=======
-            // const Item = ItemModel.findById(itemId);
-            // console.log(userId)
-            const _id = mongoose.Types.ObjectId(userId)
-            ItemModel.findByIdAndUpdate(itemId, { assignedTo: _id})
->>>>>>> 2100477ca51cc0fb16e4d54f7a5ac17e49dbfb42
             .then(callback)   
 // === Might be unecessary == Pushes to User array ==
 // .then( UserModel.findById(userId).then( user => {
